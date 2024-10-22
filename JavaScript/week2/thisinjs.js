b909b48cfdf1d973dbd100c48b6bccfc4eae6792
Taralama <tarahero65@gmail.com>
2024-10-22 14:54:49 +0545

    added comment

--- JavaScript/week2/thisinjs.js
+++ JavaScript/week2/thisinjs.js
@@ -1,38 +1,30 @@
 const person = {
-    firstName : 'Surendra',
-    lastName : 'Tamang',
-    
-    fullName :  function () {
-        console.log(this)
-        const firstName = 'Anish';
-        const lastName = 'Shrestha';
-        return this.firstName + ' ' + this.lastName
-    }
-}
+  firstName: "Surendra",
+  lastName: "Tamang",
 
-console.log(person.fullName())
+  fullName: function () {
+    console.log(this);
+    const firstName = "Anish";
+    const lastName = "Shrestha";
+    return this.firstName + " " + this.lastName;
+  },
+};
 
+console.log(person.fullName());
 
 const car = {
-    model: 'suzuki',
-    year: 2020,
-    fullDetail: function(){
-        return this // this refers to the car object 
-    }
-}
-
+  model: "suzuki",
+  year: 2020,
+  fullDetail: function () {
+    return this; // this refers to the car object
+  },
+};
 
 console.log(car.fullDetail());
 
-function show (){
-    console.log('this is show' ,this);
+function show() {
+  console.log("this is show", this);
 }
 
 show();
-
-
-
-
-
-
-
+//add new commentg
